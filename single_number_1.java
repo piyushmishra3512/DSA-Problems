a23bbdbf1843340982018553604304f7b85affa6
Piyush <piyushmishra35122006@gmail.com>
2026-08-03 22:18:16 +0530

    Create single_number_1.java

---
+++ single_number_1.java
@@ -0,0 +1,14 @@
+/*Given an array of nums of n integers. Every integer in the array appears twice except one integer. Find the number that appeared once in the array. */
+public class single_number_1 {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int num = 0;
+        for(int i = 0 ; i<n ; i++ ){
+            num = num^nums[i];
+
+        }
+        return num;
+        //your code goes here
+    }
+    
+}
